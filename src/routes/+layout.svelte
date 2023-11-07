7d09f8bae89c689aab1327e41e81e58ec7dd53b5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-07 22:56:17 +0000

    delete ununsed header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
 </svelte:head>
 
-<header class="container" />
-
 <main class="container">
 	<slot />
 </main>
